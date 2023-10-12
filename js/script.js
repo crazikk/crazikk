ebb03626ff80b661e452b25eae1e1af8d5975834
crazikk <106470303+crazikk@users.noreply.github.com>
2023-10-12 19:23:40 +0200

    script.js

--- js/script.js
+++ js/script.js
@@ -14,27 +14,33 @@ let navLinks = document.querySelectorAll('header nav a');
 let animationsPlayed = {}; // Objekt pro sledování stavu animací
 
 window.onscroll = () => {
+    let currentSection = null; // Inicializujeme aktuální sekci na null
+
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 650;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height && !animationsPlayed[id]) {
-            // active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            // active sections for animation on scroll
+        if (top >= offset && top < offset + height) {
+            // Uložíme aktuální sekci
+            currentSection = id;
+
             sec.classList.add('show-animate');
-            animationsPlayed[id] = true; // Nastavíme stav animace na true
-        }
-        // if want to use animation that repeats on scroll use this
-        else if (!animationsPlayed[id]) {
+            animationsPlayed[id] = true;
+        } else if (!animationsPlayed[id]) {
             sec.classList.remove('show-animate');
         }
     });
+
+    // Aktualizovat aktivní třídy v navigačním menu
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+
+    if (currentSection) {
+        document.querySelector('header nav a[href*=' + currentSection + ']').classList.add('active');
+    }
 
     // sticky header
     let header = document.querySelector('header');
